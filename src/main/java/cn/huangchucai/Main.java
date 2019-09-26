094ae19cd784f68df415e35e5b37ce49620ffba9
huangchucai <425205839@qq.com>
2019-09-26 18:48:24 +0800

    feat: 重构代码

--- src/main/java/cn/huangchucai/Main.java
+++ src/main/java/cn/huangchucai/Main.java
@@ -9,7 +9,6 @@ import org.apache.http.util.EntityUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -20,42 +19,49 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) throws IOException {
         List<String> linksPools = new ArrayList();
-        Set<String> handledLink = new HashSet<>();
-        String beginLink = "https://sina.cn";
-        linksPools.add(beginLink);
+        Set<String> handledLinks = new HashSet<>();
+        linksPools.add("https://sina.cn");
         while (true) {
             if (linksPools.isEmpty()) {
                 break;
             }
             String link = linksPools.remove(linksPools.size() - 1);
-            if (handledLink.contains(link)) {
+            if (handledLinks.contains(link)) {
                 continue;
             }
-
-            if ((link.contains("news.sina.cn") || link.equals(beginLink)) && !link.contains("passport.sina.cn") && !link.contains("hotnews.sina.cn")) {
-                if(link.startsWith("//")) {
-                    link = "https:" + link;
-                }
-                CloseableHttpClient httpclient = HttpClients.createDefault();
-                HttpGet httpGet = new HttpGet(link);
-                try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-                    System.out.println(response1.getStatusLine());
-                    System.out.println(link);
-                    HttpEntity entity1 = response1.getEntity();
-                    Document document = Jsoup.parse(EntityUtils.toString(entity1));
-                    Elements links = document.select("a");
-                    for (Element aTag : links) {
-                        linksPools.add(aTag.attr("href"));
-                    }
-                    ArrayList<Element> articleTags = document.select("article");
-                    if(!articleTags.isEmpty()) {
-                        for(Element acticleTag: articleTags ) {
-                            System.out.println(acticleTag.child(0).text());
-                        }
-                    }
-                    handledLink.add(link);
-                }
+            if (isInterestedLink(link)) {
+                Document document = httpGetAndParseHtml(link);
+                document.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linksPools::add);
+                storeIntoDBIfItIsNewsPage(document);
+                handledLinks.add(link);
             }
+        }
+    }
+
+    private static void storeIntoDBIfItIsNewsPage(Document document) {
+        ArrayList<Element> articleTags = document.select("article");
+        if (!articleTags.isEmpty()) {
+            for (Element acticleTag : articleTags) {
+                System.out.println(acticleTag.child(0).text());
+            }
+        }
+    }
+
+
+    private static boolean isInterestedLink(String link) {
+        return (link.contains("news.sina.cn") || link.equals("https://sina.cn")) && !link.contains("passport.sina.cn") && !link.contains("hotnews.sina.cn");
+    }
+
+    private static Document httpGetAndParseHtml(String link) {
+        if (link.startsWith("//")) {
+            link = "https:" + link;
+        }
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpGet = new HttpGet(link);
+        try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
+            HttpEntity entity1 = response1.getEntity();
+            String html = EntityUtils.toString(entity1);
+            return Jsoup.parse(html);
         }
     }
 }
